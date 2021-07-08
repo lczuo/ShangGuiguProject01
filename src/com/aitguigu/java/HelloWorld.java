93a35529f303401ba011f895880d6635d76e6564
tom_glb <goodMorning_glb@atguigu.com>
2021-07-08 16:43:09 +0800

    这是project测试

---
+++ src/com/aitguigu/java/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.aitguigu.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+
+    private int num;
+
+    public static void main(String[] args) {
+        getaa();
+    }
+
+    private static void getaa() {
+        System.out.println("HelloWorld");
+        System.out.println("11111");
+        ArrayList rr = new ArrayList();
+        Date date = new Date();
+        rr.add(0, 123);
+    }
+
+    public void dhaha(){
+        num = 10;
+    }
+}
---
+++ src/com/aitguigu/java/TemplatesTest.java
@@ -0,0 +1,11 @@
+package com.aitguigu.java;
+
+/**
+ * @className: com.aitguigu.java.TemplatesTest
+ * @description: TODO
+ * @author: lczuo
+ * @date: 2021/7/8-
+ */
+public class TemplatesTest {
+
+}
